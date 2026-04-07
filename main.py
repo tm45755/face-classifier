e5bb6ff8f48b85ef3532e33f16a1956a65e85d72
tm45755 <tm45755@gmail.com>
2026-04-08 06:02:55 +0900

    OpenCVで画像読み込みを追加

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
+import cv2
 
 folder_path = "images"
 
 image_extensions = [".jpg", ".jpeg", ".png", ".webp"]
 
-files = os.listdir(folder_path)
-
-for file in files:
+for file in os.listdir(folder_path):
     if any(file.lower().endswith(ext) for ext in image_extensions):
-        print(file)
+
+        path = os.path.join(folder_path, file)
+        img = cv2.imread(path)
+
+        if img is None:
+            print("読み込み失敗:", file)
+        else:
+            print("OK:", file, img.shape)
